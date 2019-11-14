8f2a20f699c9c1b1d8503eaf27e195c799ad714c
Neil Campbell <neildcampbell@hotmail.com>
2019-11-14 05:30:15 +0000

    added test for 4 and not 100

--- spec/leapYearsSpec.js
+++ spec/leapYearsSpec.js
@@ -4,8 +4,18 @@ describe("LEAP YEARS", function(){
         let result = leapYear(year)
         expect(result).toEqual('Yes, This is a Leap Year')
     })
-    it("should return yes if year is divisible by 100 and not 400", function(){
+    it("should return no if year is divisible by 100 and not 400", function(){
         let year = 100;
+        let result = leapYear(year)
+        expect(result).toEqual('No, This is not a Leap Year')
+    })
+    it("should return no if year is divisible by 100 and not 400", function(){
+        let year = 5;
+        let result = leapYear(year)
+        expect(result).toEqual('No, This is not a Leap Year')
+    })
+    it("should return yes if year is divisible by 4 and not 100", function(){
+        let year = 96;
         let result = leapYear(year)
         expect(result).toEqual('No, This is not a Leap Year')
     })
