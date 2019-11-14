1841016e580180c2c602551c7f348928816356e2
Neil Campbell <neildcampbell@hotmail.com>
2019-11-14 05:25:17 +0000

    tested for 100 and 400

--- spec/leapYearsSpec.js
+++ spec/leapYearsSpec.js
@@ -4,4 +4,9 @@ describe("LEAP YEARS", function(){
         let result = leapYear(year)
         expect(result).toEqual('Yes, This is a Leap Year')
     })
+    it("should return yes if year is divisible by 100 and not 400", function(){
+        let year = 100;
+        let result = leapYear(year)
+        expect(result).toEqual('No, This is not a Leap Year')
+    })
 })
--- src/leapYears.js
+++ src/leapYears.js
@@ -1,3 +1,6 @@
 function leapYear(year){
+    if(year % 100 === 0 && year % 400 !== 0){
+        return 'No, This is not a Leap Year'
+    }
     return 'Yes, This is a Leap Year'
 }
